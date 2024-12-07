6fe08a05dcc79f5e5fd1cd055176acd6df312c46
Virendra <107255416+Virendra513@users.noreply.github.com>
2024-12-07 15:11:07 +0530

    Update app33.py

--- app33.py
+++ app33.py
@@ -4,7 +4,6 @@ import numpy as np
 import torch
 from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
-from rdkit import Chem
 from mol2vec.features import mol2alt_sentence
 from gensim.models import Word2Vec
 
